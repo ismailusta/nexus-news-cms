5e7e349263c8118efa4d5d7840254b82f89932b5
ismailusta <ismail.usta028@gmail.com>
2025-07-20 02:20:08 +0300

    Vercel Blob Entegrasyonu Eklendi

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -12,6 +12,7 @@ import { Media } from './collections/Media'
 import { Categories } from './collections/Categories'
 import { Authors } from './collections/Authors'
 import { Posts } from './collections/Posts'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -34,5 +35,12 @@ export default buildConfig({
     url: process.env.DATABASE_URI || '',
   }),
   sharp,
-  plugins: [payloadCloudPlugin()],
+  plugins: [
+    vercelBlobStorage({
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN || '',
+    }),
+  ],
 })
